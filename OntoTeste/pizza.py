b907e90d2649b21c8d992b988ad6528196f84276
Bruno Souza <61155961+bruno-npc@users.noreply.github.com>
2021-09-25 12:16:07 -0300

    Agora tem resultados, mas...
    Me parece um IF feito de forma diferente...

--- OntoTeste/pizza.py
+++ OntoTeste/pizza.py
@@ -5,40 +5,44 @@ onto = get_ontology("c:/Users/Bruno/Documents/OntoTeste/pizzaBr.owl")
 onto.load()
 
 # Crie novas classes na ontologia, misturando construções OWL e métodos Python:
-print("\n### Nova classe ###")
+print("\n######################### Classes #########################")
 with onto:
-    class escolha (Thing):
-        pass
-       
     class Pizza(Thing):
         def comer(self): print("Preferencia pizza ")
 
-    class tipoPizza(Pizza >> escolha):
+    class Cobertura (Thing):
+        pass
+
+    class tem_cobertura(Pizza >> Cobertura):
       python_name = "escolhaPizza"
 
     class PizzaNaoVegetariana(Pizza):
-       equivalent_to = [
-         Pizza & tipoPizza.exactly(2, escolha)]
+       equivalent_to = [Pizza & tem_cobertura.min(4, Cobertura)]
        def comer(self): print("Eu não sou vegetariano!")
 
     class PizzaVegetariana(Pizza):
-       equivalent_to = [
-         Pizza &  tipoPizza.exactly(2, escolha)]
+       equivalent_to = [Pizza & tem_cobertura.exactly(2, Cobertura)]
        def comer(self): print("Eu sou ovolactovegetariano!")
 
 
-Tomate = escolha("pizzaTomate")
-Queijo = escolha("pizzaQueijo")
-Carne = escolha("pizzaCarne")
+print("\n############################################################################")
+Tomate = Cobertura(onto.CoberturaTomate)
+Queijo = Cobertura(onto.CoberturaQueijo)
+Carne = Cobertura(onto.CoberturaCarne)
+Peixe = Cobertura(onto.CoberturaPeixe)
 
-AllDifferent([Tomate, Queijo, Carne])
+AllDifferent([Queijo, Tomate, Carne, Peixe])
 
-pedido1 = Pizza(escolhaPizza = [Queijo, Tomate])
-pedido2 = Pizza(escolhaPizza = [Carne])
+print("\n############################################################################")
+# Pedidos com 2 ingredientes são "vegetarianos"
+pedido1 = Pizza(escolhaPizza = [Tomate, Queijo])
+
+# Pedidos com no minimo 4 ingredientes não são "vegetarianos"
+pedido2 = Pizza(escolhaPizza = [Carne, Peixe, Queijo, Tomate])
 
 close_world(Pizza)
 
-print("\n### Teste reação ###")
+print("\n############################ Teste hermit ############################")
 sync_reasoner_hermit()
 
 pedido1.comer()
